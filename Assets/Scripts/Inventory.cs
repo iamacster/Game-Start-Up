b35c387b716b6361deacd8ef04695dd3d72c5af1
a_Sharp_a <izzymacster@gmail.com>
2018-11-20 23:11:25 -0800

    Inventory Prelims & Attribute Updates
    Got the basic UI work done for the inventory and changed all player attributes to static so that external things (like items) can change them from other scripts.

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    Canvas canvas;
+    bool isDisable;
+	void Start ()
+    {
+
+        isDisable = true;
+        canvas = GetComponent<Canvas>();
+	}
+	
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.Tab))
+        {
+            if(isDisable)
+            {
+                canvas.enabled = true;
+            }
+            else
+            {
+                canvas.enabled = false;
+            }
+            isDisable = !isDisable;
+        }
+	}
+}
--- Assets/Scripts/PlayerAttributes.cs
+++ Assets/Scripts/PlayerAttributes.cs
@@ -7,21 +7,28 @@ public class PlayerAttributes : MonoBehaviour {
     public Image airaBar;
 
     //Playible Attributes
-    public float maxHealth = 1000f;
-    public float currentHealth = 1000f;
-    public float maxAira = 500f;
-    public float currentAira = 500f;
+    public static float maxHealth = 1000f;
+    public static float currentHealth = 1000f;
+    public static float maxAira = 500f;
+    public static float currentAira = 500f;
     //Stat Attributes
-    public int strength = 3;
-    public int defense = 3;
-    public int will = 3;
-    public int endurance = 3;
+    public static float strength = 0;
+    public static float defense = 0;
+    public static float will = 0;
+    public static float endurance = 0;
     //Soft Skills
     // Use this for initialization
     void Start ()
     {
-		
-	}
+        maxHealth = Mathf.Clamp(maxHealth, 0, Mathf.Infinity);
+        maxAira = Mathf.Clamp(maxAira, 0, Mathf.Infinity);
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        currentAira = Mathf.Clamp(currentAira, 0, maxAira);
+        strength = Mathf.Clamp(strength, 0, Mathf.Infinity);
+        defense = Mathf.Clamp(defense, 0, Mathf.Infinity);
+        will = Mathf.Clamp(will, 0, Mathf.Infinity);
+        endurance = Mathf.Clamp(endurance, 0, Mathf.Infinity);
+    }
 	
 	// Update is called once per frame
 	void Update ()
