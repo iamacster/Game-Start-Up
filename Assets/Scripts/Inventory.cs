374386da4e8a7442fe39b8ee707dce75aecebc7b
a_Sharp_a <izzymacster@gmail.com>
2018-11-21 16:46:46 -0800

    More Inventory Framework
    Item class set up, inventory ui reorganized

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,9 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+public enum Type { Head, Chest, Arms, Legs, Feet, Melee, Ring, Necklace };
+public class Item
+{
+    Type type;
+    float strength;
+    float defense;
+    float will;
+    float endurance;
+    float health;
+    float aira;
+    public Item(Type t, float s, float d, float w, float e, float h, float a)
+    {
+        type = t;
+        strength = s;
+        defense = d;
+        will = w;
+        endurance = e;
+        health = h;
+        aira = a;
+    }
+}
 public class Inventory : MonoBehaviour
 {
+    
     Canvas canvas;
     bool isDisable;
 	void Start ()
